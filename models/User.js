2e2c9b21764d8667dc67b7d0cd1d8762913ff271
SLUZ56 <sd54diasinacio@gmail.com>
2023-10-18 15:29:52 -0300

    first commit

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('../db/conn')
+const { Schema } = require('mongoose')
+
+const User = mongoose.model(
+    'User',
+    new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            require: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+     }, 
+     { timestamps: true},
+    )
+
+)
+module.exports = User
