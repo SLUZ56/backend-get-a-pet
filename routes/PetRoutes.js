a15474e1f3308adc4b0fbfc6bf056f9afc69cff9
SLUZ56 <sd54diasinacio@gmail.com>
2023-10-19 15:58:24 -0300

    some files was modified

--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -2,6 +2,12 @@ const router = require('express').Router()
 
 const PetController = require('../controllers/PetController')
 
-router.post('/create', PetController.create)
+// middlewares
+const verifyToken = require('../helpers/verify-token')
+const { imageUpload } = require('../helpers/image-upload')
+
+router.post('/create', verifyToken, imageUpload.array('images'), PetController.create)
+router.get('/', PetController.getAll)
+router.get('/mypets', verifyToken, PetController.getAllUserPets)
 
 module.exports = router
