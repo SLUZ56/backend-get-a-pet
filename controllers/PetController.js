df26dd0d855a6d5c5f3a5f72436f447e722eb267
SLUZ56 <sd54diasinacio@gmail.com>
2023-10-22 22:37:13 -0300

    some files modified

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -3,6 +3,8 @@ const Pet = require('../models/Pet')
 // helpers
 const getToken = require('../helpers/get-token')
 const getUserByToken = require('../helpers/get-user-by-token')
+const ObjectId = require('mongoose').Types.ObjectId
+
 
 module.exports = class PetController {
 
@@ -94,5 +96,145 @@ module.exports = class PetController {
       pets: pets
     })
 
+  }
+
+  static async getAllUserAdoptions(req, res) {
+    // get user from token
+    const token = getToken(req)
+    const user  = await getUserByToken(token)
+
+    const pets = await Pet.find({'adopter._id': user._id}).sort('-createdAt')
+
+    res.status(200).json({
+      pets: pets
+    })
+  }
+
+  static async getPetById(req, res) {
+     const id = req.params.id
+
+     // check if id is valid
+     if(!ObjectId.isValid(id)) {
+       res.status(422).json({ message: 'ID inválido!'})
+       return
+     }
+
+     // check if pet exists
+     const pet = await Pet.findOne({_id: id})
+
+     if(!pet) {
+       res.status(404).json({ message: 'Pet não encontrado!'})
+     }
+
+     res.status(200).json({
+      pet: pet,
+     })
+
+  }
+
+  static async removePetById(req, res) {
+    const id = req.params.id
+
+    // check if id is valid
+    if(!ObjectId.isValid(id)) {
+      res.status(422).json({ message: 'ID inválido!'})
+      return
+    }
+
+    // check if pet exists
+    const pet = await Pet.findOne({_id: id})
+
+    if(!pet) {
+      res.status(404).json({ message: 'Pet não encontrado!'})
+      return
+    }
+
+    // check if logged in user registered the pet
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+
+    if(pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({ message: 'Houve um problema em processar a sua solicitação, tente novamente mais tarde!'})
+      return
+    }
+
+    await Pet.findByIdAndRemove(id)
+
+    res.status(200).json({
+      message: 'Pet removido com sucesso!'
+     })
+
+  }
+
+  static async updatePet(req, res) {
+    const id = req.params.id
+
+    const { name, age, weight, color, available } = req.body 
+
+    const images = req.files
+
+    const updateData = {}
+
+     // check if pet exists
+     const pet = await Pet.findOne({_id: id})
+
+     if(!pet) {
+       res.status(404).json({ message: 'Pet não encontrado!'})
+       return
+     }
+
+      // check if logged in user registered the pet
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+
+    if(pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({ message: 'Houve um problema em processar a sua solicitação, tente novamente mais tarde!'})
+      return
+    }
+
+      // validations
+      if(!name) {
+        res.status(422).json({message: "O nome é obrigatório!"})
+        return
+      } else {
+        updateData.name = name
+      }
+
+      if(!age) {
+        res.status(422).json({message: "O idade é obrigatória!"})
+        return
+      } else {
+        updateData.age = age
+      }
+
+      if(!weight) {
+        res.status(422).json({message: "O peso é obrigatório!"})
+        return
+      } else {
+        updateData.weight = weight
+      }
+
+      if(!color) {
+        res.status(422).json({message: "O cor é obrigatória!"})
+        return
+      } else {
+        updateData.color = color
+      } 
+
+      if(images.length === 0) {
+        res.status(422).json({message: "A imagem é obrigatória!"})
+        return
+      } else {
+        updateData.images = []
+        images.map((image) => {
+          updateData.images.push(image.filename)
+        })
+      }
+
+      await Pet.findByIdAndUpdate(id, updateData)
+
+      res.status(200).json({ message: 'Pet atualizado com sucesso!'})
   }
 }
